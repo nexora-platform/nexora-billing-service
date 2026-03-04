c07a8c8d1f9b2ce3a989ae188d380418d56b3b19
Ali Al-Jalo <ali.aljalo@gmail.com>
2026-03-04 18:08:23 +0300

    chore: initialize billing service project

---
+++ src/main/java/com/nexora/billing/NexoraBillingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.nexora.billing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexoraBillingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexoraBillingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nexora/billing/NexoraBillingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nexora.billing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexoraBillingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

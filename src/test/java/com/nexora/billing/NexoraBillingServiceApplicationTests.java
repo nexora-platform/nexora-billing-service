42957dd07ac0ae5687f008b14904171c049f698d
Ali Al-Jalo <ali.aljalo@gmail.com>
2026-03-06 19:31:41 +0300

    test: add Testcontainers PostgreSQL for integration tests

--- src/test/java/com/nexora/billing/NexoraBillingServiceApplicationTests.java
+++ src/test/java/com/nexora/billing/NexoraBillingServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.nexora.billing;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
+@Import(TestPostgresContainer.class)
 class NexoraBillingServiceApplicationTests {
 
 	@Test
